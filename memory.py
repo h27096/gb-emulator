0005ac79c50ea206cd26444d11dd67ad9833a1ae
h27096 <h27096@gmail.com>
2026-04-24 06:16:45 -0500

    Implement Memory class for RAM and ROM handling

--- memory.py
+++ memory.py
@@ -1,1 +1,15 @@
+class Memory:
+    def __init__(self, rom_path):
+        self.ram = [0] * 65536  # 64KB
 
+        with open(rom_path, "rb") as f:
+            rom_data = f.read()
+
+        for i in range(len(rom_data)):
+            self.ram[i] = rom_data[i]
+
+    def read(self, addr):
+        return self.ram[addr]
+
+    def write(self, addr, value):
+        self.ram[addr] = value & 0xFF
