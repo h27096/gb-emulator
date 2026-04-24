f84a65ec1785ba689a9cfd31dde4d6127cec3d92
h27096 <h27096@gmail.com>
2026-04-24 06:27:20 -0500

    Delete imput.py

--- imput.py
+++
@@ -1,8 +0,0 @@
-import pygame
-
-class Input:
-    def handle(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-        return True
